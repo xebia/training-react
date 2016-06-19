02db72b644f838d863b912f0d43692c81331ad29
Ruben Oostinga <roostinga@xebia.com>
2016-06-19 14:02:35 +0200

    Add messages for loading and no giphy found

--- redux-giphy-search/giphy-search/giphy-response-reducer.js
+++ redux-giphy-search/giphy-search/giphy-response-reducer.js
@@ -2,8 +2,13 @@ const initialState = { message: 'Use the search bar to find giphy\'s' };
 export default function giphyResponseReducer(state = initialState, action) {
   const { type, giphyList, error } = action;
   switch (type) {
+    case 'SUBMIT_SEARCH':
+      return { message: 'Loading...' };
     case 'GIPHY_RESPONSE':
-      return { giphyList };
+      if (giphyList.length) {
+        return { giphyList };
+      }
+      return { message: 'No giphy found' };
     case 'GIPHY_ERROR':
       return { message: `Failed to search, got error: ${error}` };
     default:

3e9ffba847ddd8722599e5022122b51646aa8786
Ruben Oostinga <roostinga@xebia.com>
2016-06-24 22:19:03 +0200

    Remove connect redux to react solution

--- connect-redux-to-react/app/App.js
+++ connect-redux-to-react/app/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import store from './store.js';
 import RandomImagePage from '../random-image/RandomImagePage.js';
 
 export default () => (
-  <Provider store={store}>
-    <RandomImagePage />
-  </Provider>
+  <RandomImagePage />
 );
 

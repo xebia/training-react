1382bf1c2705bf7530b8500ea49629c5e51737a5
Ruben Oostinga <roostinga@xebia.com>
2016-06-24 17:46:40 +0200

    Revert "Remove react-form solution"
    This reverts commit 002dd1e1a900070f1dd9b1e172664a58d37646ff.

--- react-form/react-form/ReactFormPage.js
+++ react-form/react-form/ReactFormPage.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ReactFormPage = () => (
-  <form>
-  Form to be added
-  </form>
-);
+class ReactFormPage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    this.onChangeName = e => this.setState({
+      ...this.state,
+      name: e.target.value,
+    });
+
+    this.onChangeGender = e => this.setState({
+      ...this.state,
+      gender: e.target.value,
+    });
+  }
+
+  render() {
+    const {
+      name,
+      gender,
+    } = this.state;
+
+    return (
+      <form>
+        <p>Name: <input name="name" value={name} onChange={this.onChangeName} /></p>
+        <p>Gender:
+          m
+          <input
+            name="gender" type="radio" value="m"
+            checked={gender === 'm'} onChange={this.onChangeGender}
+          />
+          f
+          <input
+            name="gender" type="radio" value="f"
+            checked={gender === 'f'} onChange={this.onChangeGender}
+          />
+        </p>
+        <pre>state = {JSON.stringify(this.state, null, ' ')}</pre>
+      </form>
+    );
+  }
+}
 
 export default ReactFormPage;

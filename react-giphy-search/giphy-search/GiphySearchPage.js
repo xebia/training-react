fce49915bc412bc628c13600c27f60b7a42a7267
Ruben Oostinga <roostinga@xebia.com>
2016-06-24 17:50:51 +0200

    Remove react-giphy-search-solution

--- react-giphy-search/giphy-search/GiphySearchPage.js
+++ react-giphy-search/giphy-search/GiphySearchPage.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { searchGiphy } from './giphy-search-service.js';
+// import { searchGiphy } from './giphy-search-service.js';
 
 const GiphyListView = ({
   message,
@@ -27,48 +27,16 @@ GiphyListView.propTypes = {
 };
 
 class GiphySearchPage extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      message: 'Use the search bar to find giphy\'s',
-      searchTerm: '',
-    };
-
-    this.onChangeSearchTerm = e => this.setState({
-      ...this.state,
-      searchTerm: e.target.value,
-    });
-
-    this.onSubmitSearch = e => {
-      // prevents a page refresh
-      e.preventDefault();
-      const { searchTerm } = this.state;
-      searchGiphy(searchTerm)
-      .then(
-        ({ data }) => this.setState({ giphyList: data, message: undefined })
-      )
-      .catch(
-        (error) => this.setState({ message: error.toString(), giphyList: undefined })
-      );
-    };
-  }
-
   render() {
-    const {
-     searchTerm,
-     ...other,
-    } = this.state;
-
     return (
       <div>
-        <form onSubmit={this.onSubmitSearch}>
+        <form>
           <p>
             Search:
-            <input name="searchTerm" value={searchTerm} onChange={this.onChangeSearchTerm} />
+            <input name="searchTerm" />
           </p>
         </form>
-        <GiphyListView {...other} />
+        <GiphyListView message="To be implemented" />
         <pre>state = {JSON.stringify(this.state, null, ' ')}</pre>
       </div>
     );

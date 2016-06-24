2c71fb98e9f87014b301f0a86d4a2d8a08f8244f
Ruben Oostinga <roostinga@xebia.com>
2016-06-24 22:19:25 +0200

    Revert "Remove connect redux to react solution"
    This reverts commit 3e9ffba847ddd8722599e5022122b51646aa8786.

--- connect-redux-to-react/app/App.js
+++ connect-redux-to-react/app/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from './store.js';
 import RandomImagePage from '../random-image/RandomImagePage.js';
 
 export default () => (
-  <RandomImagePage />
+  <Provider store={store}>
+    <RandomImagePage />
+  </Provider>
 );
 

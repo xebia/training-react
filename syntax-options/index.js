2338e49987613a868a8beef639d052da81949679
Gert Hengeveld <info@ghengeveld.nl>
2016-06-27 08:19:31 +0530

    Solution.

--- syntax-options/index.js
+++ syntax-options/index.js
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export ...
+export const Classic = React.createClass({
+  render() {
+    return (
+      <div>Hello world from classic component</div>
+    );
+  }
+});
+
+export class Classy extends React.Component {
+  render() {
+    return (
+      <div>Hello world from classy component</div>
+    );
+  }
+}
+
+export function Functional() {
+  return (
+    <div>Hello world from functional component</div>
+  );
+}

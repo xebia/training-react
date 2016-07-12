73e084bf829083c3ac0bcb24241e3a7bed58faae
Ruben Oostinga <roostinga@xebia.com>
2016-07-12 14:36:05 +0200

    Remove setup redux thunk

--- redux-giphy-search/app/store.js
+++ redux-giphy-search/app/store.js
@@ -1,5 +1,5 @@
 import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+// import thunkMiddleware from 'redux-thunk';
 
 import giphySearchTermReducer from '../giphy-search/giphy-searchterm-reducer.js';
 import giphyResponseReducer from '../giphy-search/giphy-response-reducer.js';
@@ -11,9 +11,6 @@ const combinedReducers = combineReducers({
   giphyResponse: giphyResponseReducer,
 });
 
-const store = compose(
-  applyMiddleware(thunkMiddleware),
-  debugStoreEnhancer
-)(createStore)(combinedReducers);
+const store = debugStoreEnhancer(createStore)(combinedReducers);
 
 export default store;

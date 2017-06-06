0e6b4fb2b27061dae7b14c923d6977a47d318099
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-06 20:17:22 +0200

    Fix timer spec

--- src/es6-jsx-examples/timer/__tests__/TimerSpec.js
+++ src/es6-jsx-examples/timer/__tests__/TimerSpec.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Timer from '../Timer.js';
+import Timer from '../Timer';
+
+jest.useFakeTimers();
+Date = jest.fn();
+Date.now = jest.fn();
 
 describe('Component: Timer', () => {
   let wrapper;
-  let sandbox;
 
   beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-    sandbox.useFakeTimers();
-    sandbox.spy(global, 'setInterval');
-    sandbox.spy(global, 'clearInterval');
+    jest.clearAllMocks();
+    setInterval.mockRestore();
     wrapper = shallow(<Timer />);
   });
-
+  
   afterEach(() => {
-    sandbox.restore();
+    jest.clearAllTimers();
   });
 
   it('should render the current time based on the state', () => {
     wrapper.setState({ time: new Date(10000) });
-    expect(wrapper.find('p').text()).toBe(`The time is ${new Date(10000)}`);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should update state.time every 500ms', () => {
-    expect(wrapper.state()).toEqual({ time: new Date(0) });
-    sandbox.clock.tick(499);
-    expect(wrapper.state()).toEqual({ time: new Date(0) });
-    sandbox.clock.tick(1);
-    expect(wrapper.state()).toEqual({ time: new Date(500) });
-    sandbox.clock.tick(500);
-    expect(wrapper.state()).toEqual({ time: new Date(1000) });
+    expect(wrapper.state().time).toBe(Date.mock.instances[0]);
+    jest.runTimersToTime(499);
+    expect(wrapper.state().time).toBe(Date.mock.instances[0]);
+    jest.runTimersToTime(1);
+    expect(wrapper.state().time).toBe(Date.mock.instances[1]);
+    jest.runTimersToTime(500);
+    expect(wrapper.state().time).toBe(Date.mock.instances[2]);
   });
 
   it('should clear the interval on unmount', () => {
-    expect(setInterval).to.have.callCount(1);
+    expect(clearInterval).not.toHaveBeenCalled();
     wrapper.unmount();
-    expect(clearInterval).to.have.been.calledWithExactly(setInterval.returnValues[0]);
+    expect(clearInterval).toHaveBeenCalled();
   });
 });

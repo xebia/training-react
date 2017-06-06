0eac9f0f8e51277bd8a3b2da1ec9c23d5943ac2a
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-06 19:20:32 +0200

    Use a snapshot test for helloworld

--- src/es6-jsx-examples/helloworld/__tests__/HelloWorldSpec.js
+++ src/es6-jsx-examples/helloworld/__tests__/HelloWorldSpec.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import HelloWorld from '../HelloWorld.js';
 
 describe('Component: HelloWorld', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<HelloWorld />);
-  });
-
-  it('should contain the text "Hello World"', () => {
-    expect(wrapper).to.contain.text('Hello World');
-  });
-
-  it('should render a list of 3 items', () => {
-    expect(wrapper.find('li').map(e => e.text())).to.deep.equal(['item 1', 'item 2', 'item 3']);
+  it('should render correctly', () => {
+    const tree = renderer.create(
+      <HelloWorld />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });

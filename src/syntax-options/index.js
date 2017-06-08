a6db7113046b79a2462c74dce60998d1c5744a8a
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-08 17:42:39 +0200

    Revert "Remove syntax solution"
    This reverts commit e3bcce2c2ea1726d7dc6488a1849ac8eb8dde99e.

--- src/syntax-options/index.js
+++ src/syntax-options/index.js
@@ -8,3 +8,17 @@ export const Classic = createReactClass({
     );
   }
 });
+
+export class Classy extends React.Component {
+  render() {
+    return (
+      <div>Hello world from classy component</div>
+    );
+  }
+}
+
+export function Functional() {
+  return (
+    <div>Hello world from functional component</div>
+  );
+}

3524e1e851f6e9af614240fb9c3e5c2dfa05b6fd
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-08 17:40:28 +0200

    Remove jsx solution

--- src/jsx-exercise/example-page/ExamplePage.js
+++ src/jsx-exercise/example-page/ExamplePage.js
@@ -1,38 +1,9 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
-import Navigation from './Navigation.js';
-import Jumbotron from './Jumbotron.js';
-import Block from './Block.js';
-import Footer from './Footer.js';
-
-const ExamplePage = ({
-  project,
-  navigationLinks,
-  jumbotron,
-  blocks,
-  footer,
-}) => (
+const ExamplePage = () => (
   <div>
-    <Navigation project={project} links={navigationLinks} />
-    <Jumbotron {...jumbotron} />
-    <div className="container">
-      {/* Example row of columns */}
-      <div className="row">
-        {blocks.map((block, index) => <Block key={index} {...block} />)}
-      </div>
-      <hr />
-      <Footer {...footer} />
-    </div> {/* /container */}
+    Implement me!
   </div>
 );
-
-ExamplePage.propTypes = {
-  project: PropTypes.string.isRequired,
-  navigationLinks: PropTypes.array.isRequired,
-  jumbotron: PropTypes.object.isRequired,
-  blocks: PropTypes.array.isRequired,
-  footer: PropTypes.object.isRequired,
-};
 
 export default ExamplePage;

59e9d702ed1c5bd8753f7eae3ee4a73cf3b440dc
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-08 17:40:28 +0200

    Revert "Remove jsx solution"
    This reverts commit 3524e1e851f6e9af614240fb9c3e5c2dfa05b6fd.

---
+++ src/jsx-exercise/example-page/Block.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Button from './Button.js';
+
+const Block = ({
+  heading,
+  body,
+  button,
+}) => (
+  <div className="col-md-4">
+    <h2>{heading}</h2>
+    <p>{body}</p>
+    <p><Button classNames="btn-secondary" {...button} /></p>
+  </div>
+);
+
+Block.propTypes = {
+  heading: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  button: PropTypes.object.isRequired,
+};
+
+export default Block;
---
+++ src/jsx-exercise/example-page/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Button = ({
+  text,
+  href,
+  classNames,
+}) => (
+  <a className={`btn ${classNames}`} href={href} role="button">{text}</a>
+);
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  classNames: PropTypes.string.isRequired,
+};
+
+
+export default Button;
